8edbd6f0253e77e516afe7f9fa52c8c01c4e4b38
sw.oh <sw.oh@gnewsoft.com>
2024-05-17 17:53:31 +0900

    Initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WwxhTPv4aYO38FJfIzc26XB1n8kxLtFoEjFlE9R4qE4=\"\n}"
---
+++ app/(home)/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Page() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState();
+    const getMovies = async () => {
+        const response = await fetch(
+            "https://nomad-movies.nomadcoders.workers.dev/movies"
+        );
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            <h1>{isLoading ? "Loading..." : JSON.stringify(movies)}</h1>
+        </div>
+    );
+}
