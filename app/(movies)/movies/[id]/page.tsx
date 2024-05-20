6b23182ed4598cbb447575dbeea3336125e10984
sw.oh <sw.oh@gnewsoft.com>
2024-05-20 13:36:52 +0900

    style : css 모듈 설정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,24 +1,27 @@
-import { API_URL } from "../../../(home)/page";
+import { Suspense } from "react";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
-async function getMovie(id: string) {
-    console.log(`Fetching movies: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-
-async function getVideos(id: string) {
-    console.log(`Fetching videos: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
-}
-
-export default async function MovieDetail({
-    params: { id },
-}: {
+interface IParams {
     params: { id: string };
-}) {
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    return <h1>{movie.title}</h1>;
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
+    return (
+        <div>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    );
 }
